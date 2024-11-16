338efc7fde21b98b27e591e14498f435ec9cd09d
brain-in-use <senaakash008@gmail.com>
2024-11-16 15:44:21 +0530

    after video 27

--- src/components/News.js
+++ src/components/News.js
@@ -8,8 +8,8 @@ export class News extends Component {
         <div className="card my-3" style={{width: "18rem"}}>
   <img src={imageUrl} className="card-img-top" alt="Loading Er"/>
   <div className="card-body">
-    <h5 className="card-title">{title.substring(0,30)+"..."}</h5>
-    <p className="card-text">{description.substring(0,80)+"..."}</p>
+    <h5 className="card-title">{title?title.substring(0,30):""+"..."}</h5>
+    <p className="card-text">{description?description.substring(0,80):""+"..."}</p>
     <a href={url}target="_blank" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,269 +1,30 @@
 import React, { Component } from "react";
 import News from "./News";
 export class NewsItems extends Component {
-  article = [
-    {
-      "source": {
-          "id": "al-jazeera-english",
-          "name": "Al Jazeera English"
-      },
-      "author": "Al Jazeera Staff",
-      "title": "South Africa vs India – T20 series: Match times, squads, head-to-head, news",
-      "description": "Suryakumar Yadav leads a new-look Indian squad in a four-match T20 series as the Proteas seek to avenge the final loss.",
-      "url": "https://www.aljazeera.com/sports/2024/11/7/south-africa-vs-india-t20-series-match-times-squads-head-to-head-news",
-      "urlToImage": "https://www.aljazeera.com/wp-content/uploads/2024/11/GettyImages-2182694841-1730994027.jpg?resize=1920%2C1440",
-      "publishedAt": "2024-11-07T16:05:24Z",
-      "content": "The wounds of a painful loss at the ICC T20 World Cup final will still be fresh for South Africa as they face their conquerors India in a four-match T20 series at home starting on Friday.\r\nMore than … [+4473 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "Neowin"
-      },
-      "author": "Sayan Sen",
-      "title": "TECH_BARGAINS 24/10: Nvidia GTX 1650, external SSDs, Sony Samsung LG TVs, and more",
-      "description": "In today's TECH_BARGAINS, we cover some of the best deals of the day, including Samsung and LG QLED / OLED TVs, Apple iPhone, Google Pixel, Nvidia GTX 1650, external SSDs, and more. Read more...",
-      "url": "https://www.neowin.net/deals/techbargains-2410-nvidia-gtx-1650-external-ssds-sony-samsung-lg-tvs-and-more/",
-      "urlToImage": "https://cdn.neowin.com/news/images/uploaded/2024/10/1729753166_tech_baragains_story.jpg",
-      "publishedAt": "2024-10-24T07:16:01Z",
-      "content": "Aside from those, you can also check out Sony, Samsung, and LG TVs, Nvidia's GTX 1650 GPU, Google Pixel, Apple iPhones, external SSDs and more. The full list of featured deals is given below:<li>ZYXE… [+3607 chars]"
-  },
-  {
-      "source": {
-          "id": "le-monde",
-          "name": "Le Monde"
-      },
-      "author": "Carole Dieterich",
-      "title": "Decathlon, l’entreprise française qui a conquis les Indiens",
-      "description": "L’enseigne tricolore, qui emploie 5 700 personnes en Inde, est appréciée pour ses petits prix. Elle a vu ses ventes bondir localement de 37 % en 2023 et compte investir 100 millions d’euros en cinq ans pour se développer en ligne et dans les villes moyennes.",
-      "url": "https://www.lemonde.fr/economie/article/2024/10/29/decathlon-l-entreprise-francaise-qui-a-conquis-les-indiens_6364587_3234.html",
-      "urlToImage": "https://img.lemde.fr/2024/10/28/0/3/2994/1996/1440/960/60/0/050afac_1730104344098-hemis-2k31cat.jpg",
-      "publishedAt": "2024-10-29T08:30:09Z",
-      "content": "Dans un magasin Decathlon dUluberia (Bengale-Occidental), en Inde, le 26 octobre 2020. ALAMY STOCK PHOTO\r\nHormis le cricket, dont on dit ici quil est une religion, lInde ne fait pas figure de grande … [+2632 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "[Removed]"
-      },
-      "author": null,
-      "title": "[Removed]",
-      "description": "[Removed]",
-      "url": "https://removed.com",
-      "urlToImage": null,
-      "publishedAt": "2024-10-29T18:59:11Z",
-      "content": "[Removed]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "[Removed]"
-      },
-      "author": null,
-      "title": "[Removed]",
-      "description": "[Removed]",
-      "url": "https://removed.com",
-      "urlToImage": null,
-      "publishedAt": "2024-10-23T07:04:48Z",
-      "content": "[Removed]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "Plos.org"
-      },
-      "author": "Xiaoyang Pan, Kim Geok Soh, Kim Lam Soh",
-      "title": "Viable strategies for enhancing performance in ball sports by mitigating mental fatigue: A systematic review",
-      "description": "Ball sports demand precise control of body and ball while executing tactics and team coordination, which leads to cognitive depletion and mental fatigue. The detrimental effects of mental fatigue on physical, technical, cognitive, and tactical performance in …",
-      "url": "https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0313105",
-      "urlToImage": "https://journals.plos.org/plosone/article/figure/image?id=10.1371/journal.pone.0313105.g001&size=inline",
-      "publishedAt": "2024-11-08T14:00:00Z",
-      "content": "Abstract\r\nBall sports demand precise control of body and ball while executing tactics and team coordination, which leads to cognitive depletion and mental fatigue. The detrimental effects of mental f… [+28936 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "Publishingperspectives.com"
-      },
-      "author": "Porter Anderson",
-      "title": "China Bestsellers in September: Kids’ Books Ahead of Shanghai Book Fair",
-      "description": "A bid for children's success boosts 'comic+' and other books for kids among parents, something seen in the China's September bestseller lists.\nThe post China Bestsellers in September: Kids’ Books Ahead of Shanghai Book Fair appeared first on Publishing Perspe…",
-      "url": "https://publishingperspectives.com/2024/11/china-bestsellers-in-september-kids-books-ahead-of-shanghai-book-fair/",
-      "urlToImage": "https://publishingperspectives.com/wp-content/uploads/2024/11/710-TEASE-iStock-1215911459-Mark-Zhu-Hangzhou-bookshop-in-Qiaoxi-historical-district-near-Beijing-Hangzhou-Grand-Canal-ftw.jpg",
-      "publishedAt": "2024-11-06T21:46:39Z",
-      "content": "A bid for children’s success boosts ‘comic+’ and other books for kids among parents, something seen in the China’s September bestseller lists.\r\n A bookstore in Hangzhou, set in the Qiaoxi historical … [+28534 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "Thefutoncritic.com"
-      },
-      "author": null,
-      "title": "Freeform's \"25 Days of Christmas\" Returns with Yuletide Cheer This December",
-      "description": "A delightful collection of Christmas favorites is being offered to make spirits bright, including \"Home Alone,\" \"The Santa Clause\" trilogy and \"Dr. Seuss' How the Grinch Stole Christmas\" (2000).",
-      "url": "http://www.thefutoncritic.com/news/2024/11/01/freeforms-25-days-of-christmas-returns-with-yuletide-cheer-this-december-36211/20241101freeform01/",
-      "urlToImage": null,
-      "publishedAt": "2024-11-01T09:06:00Z",
-      "content": "DECK THE HALLS WITH BOUGHS OF FREEFORM! \r\n'25 DAYS OF CHRISTMAS' RETURNS WITH YULETIDE CHEER THIS DECEMBER\r\nHoliday Classics Include 'Dr. Seuss' How the Grinch Stole Christmas,' 'Miracle on 34th Stre… [+12798 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "Thefutoncritic.com"
-      },
-      "author": null,
-      "title": "Disney Channel's \"Fa-La-La-Lidays\" and Disney Jr.'s \"Magical Holidays\" Unwrap Festive Programming for the Whole Family",
-      "description": "Highlights of \"Disney Channel's Fa-la-la-lidays\" include new animated holiday episodes of \"Big City Greens\" and \"ZOMBIES: The Re-Animated Series.\"",
-      "url": "http://www.thefutoncritic.com/news/2024/11/13/disney-channels-fa-la-la-lidays-and-disney-jrs-magical-holidays-unwrap-festive-programming-for-the-whole-family-921214/20241113disney01/",
-      "urlToImage": null,
-      "publishedAt": "2024-11-13T09:01:00Z",
-      "content": "DISNEY CHANNEL'S 'FA-LA-LA-LIDAYS' AND \r\nDISNEY JR.'S 'MAGICAL HOLIDAYS' UNWRAP FESTIVE PROGRAMMING FOR THE WHOLE FAMILY \r\nOn Disney Channel, New Themed Episodes of 'Big City Greens' and 'ZOMBIES: Th… [+7413 chars]"
-  },
-  {
-      "source": {
-          "id": "the-times-of-india",
-          "name": "The Times of India"
-      },
-      "author": "PTI",
-      "title": "1st T20I: India's second line stars look for a breakaway series against South Africa",
-      "description": "India's second-string T20I squad, featuring Sanju Samson, Abhishek Sharma, and others, aims to solidify their places in the four-match series against South Africa.  The series presents an opportunity for these players to showcase their skills and stake their …",
-      "url": "https://economictimes.indiatimes.com/news/sports/1st-t20i-indias-second-line-stars-look-for-a-breakaway-series-against-south-africa/articleshow/115045928.cms",
-      "urlToImage": "https://img.etimg.com/thumb/msid-115045973,width-1200,height-630,imgsize-32448,overlay-economictimes/articleshow.jpg",
-      "publishedAt": "2024-11-07T07:24:12Z",
-      "content": "As India firmly embraces a transition phase, a clutch of second line stars such as Sanju Samson and Abhishek Sharma will be hoping for a breakaway series in the four T20Is against South Africa, start… [+3711 chars]"
-  },
-  {
-      "source": {
-          "id": "national-geographic",
-          "name": "National Geographic"
-      },
-      "author": "Pol O Conghaile",
-      "title": "What to see and do in Dublin, Ireland",
-      "description": "From Temple Bar to the Book of Kells, the Irish capital has its fair share of iconic attractions, but you’ll find just as much magic elsewhere — in its thriving music scene, new breed of after-hour spaces and the coastline right on its doorstep.",
-      "url": "https://www.nationalgeographic.com/travel/article/guide-to-dublin-ireland",
-      "urlToImage": "https://i.natgeofe.com/n/bcf94080-432d-4229-8432-813b47724403/dublin2resize_16x9.jpg?w=1200",
-      "publishedAt": "2024-10-22T00:00:00Z",
-      "content": "Dublin is a city thats impossible to do. It has its big hits: theres the Guinness Storehouse, where the black stuff is brewed, tasted and revered; and there are taprooms that thump in the party distr… [+11659 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "In Praising Kohli, Ex-India Star's Dig At Sachin, Ganguly: \"They Never...\"",
-      "description": "Sanjay Manjrekar hailed Virat Kohli for his decision to promote himself in the India vs New Zealand 1st Test, while taking a shot at former legends Sachin Tendulkar and Sourav Ganguly.",
-      "url": "https://sports.ndtv.com/india-vs-new-zealand-2024/in-praising-virat-kohli-ex-india-stars-dig-at-sachin-tendulkar-sourav-ganguly-they-never-wanted-6808122",
-      "urlToImage": "https://c.ndtvimg.com/2019-02/1s9q5mgo_sachin-tendulkar-sourav-ganguly-afp_625x300_24_February_19.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-10-17T10:22:35Z",
-      "content": "Virat Kohli's poor Test form in 2024 continued, as he got out for a duck in the first innings against New Zealand. In spite of that, Kohli - who promoted himself to No. 3 due to Shubman Gill's absenc… [+2019 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "\"That's Pakistan Cricket...\": Stokes' Honest Take On Babar, Shaheen's Snub",
-      "description": "Pakistan have dropped some big names ahead of the second Test, including former captain Babar Azam and star pacer Shaheen Shah Afridi.",
-      "url": "https://sports.ndtv.com/cricket/thats-pakistan-cricket-ben-stokes-responds-to-babar-azam-shaheen-afridis-snub-question-6787873",
-      "urlToImage": "https://c.ndtvimg.com/2024-10/q8hg37t8_ben-stokes-afp_625x300_14_October_24.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-10-15T01:53:26Z",
-      "content": "England captain Ben Stokes is set to make his return to competitive cricket after two months during the second Test against Pakistan in Multan, starting October 15. Stokes, who had picked up a hamstr… [+2197 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "No Birthday Wish From RCB For Overseas IPL Star. Internet Can't Keep Calm",
-      "description": "Australian cricket team all-rounder Glenn Maxwell celebrated his 36th birthday on Monday and the social media was filled with wishes for the explosive batter.",
-      "url": "https://sports.ndtv.com/cricket/no-birthday-wish-from-rcb-for-overseas-ipl-star-internet-cant-keep-calm-6794214",
-      "urlToImage": "https://c.ndtvimg.com/2024-05/ou5k9plg_rcb-bcci_625x300_19_May_24.jpg?im=FitAndFill,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-10-15T14:19:10Z",
-      "content": "Australian cricket team all-rounder Glenn Maxwell celebrated his 36th birthday on Monday and the social media was filled with wishes for the explosive batter. However, there was no birthday wish from… [+2418 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "Asian News International",
-      "title": "MS Dhoni To Play T10 Cricket? T Ten Global Chairman Says \"We Can Expect...\"",
-      "description": "T Ten Global Sports chairman Shaji Ul Mulk did not remove the possibility of MS Dhoni playing T10 cricket.",
-      "url": "https://sports.ndtv.com/cricket/ms-dhoni-to-play-t10-cricket-t-ten-global-chairman-says-we-can-expect-6824846",
-      "urlToImage": "https://c.ndtvimg.com/2024-09/kmh89fd8_ms-dhoni-bcci_625x300_15_September_24.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-10-20T13:44:53Z",
-      "content": "Shaji Ul Mulk, T Ten Global Sports Founder and Chairman, believes the prospect of seeing India icon MS Dhoni in the T10 format could be a possibility. Meanwhile, uncertainty continues to loom over MS… [+2306 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "Pak's New Batting Sensation Was Slapped By Rauf On Field. Old Video Viral",
-      "description": "Pakistan cricket team batter Kamran Ghulam impressed everyone with a brilliant century on debut against England on Tuesday.",
-      "url": "https://sports.ndtv.com/cricket/pakistans-new-batting-sensation-kamran-ghulam-was-slapped-by-haris-rauf-on-field-old-video-goes-viral-6797698",
-      "urlToImage": "https://c.ndtvimg.com/2024-10/c8qbs43o_rauf-kamran-slap_625x300_16_October_24.jpeg",
-      "publishedAt": "2024-10-16T03:48:40Z",
-      "content": "Pakistan cricket team batter Kamran Ghulam impressed everyone with a brilliant century on debut against England on Tuesday. The 29-year-old looked in brilliant form against the England bowlers on Day… [+1855 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "Harbhajan's Reply To Fan's \"Earlier Generation\" Post After India's Defeat",
-      "description": "Team India faced an embarrassing and historic clean sweep against New Zealand in a three-match Test series at home.",
-      "url": "https://sports.ndtv.com/india-vs-new-zealand-2024/harbhajans-fiery-reply-to-fans-earlier-generation-played-murali-warne-post-after-0-3-defeat-6938730",
-      "urlToImage": "https://c.ndtvimg.com/2023-04/or8bmvc8_harbhajan-singh-bcci_625x300_20_April_23.jpg?im=FitAndFill,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-11-06T06:00:27Z",
-      "content": "Just some days before the highly anticipated Border-Gavaskar Trophy in Australia, Team India faced an embarrassing and historic clean sweep against New Zealand in a three-match Test series at home. R… [+2427 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "BCCI Under Fire From Fan For Poor Stadium Facility During India vs NZ Test",
-      "description": "India vs New Zealand: Consistent rain meant that despite a good drainage system at the M Chinnaswamy Stadium,play was not possible on Day 1 of the first Test",
-      "url": "https://sports.ndtv.com/india-vs-new-zealand-2024/bcci-under-fire-from-fan-who-claims-he-suffered-from-poor-stadium-facility-in-bengaluru-during-india-vs-nz-test-6803572",
-      "urlToImage": "https://c.ndtvimg.com/2024-10/tgidgffo_virat-kohli-m-chinnaswamy_625x300_16_October_24.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-10-16T12:44:01Z",
-      "content": "Fans in Bengaluru had a bad day as play was not possible on the first day of the first Test between India and New Zealand at the M Chinnaswamy Stadium. Consistent rain meant that despite a good drain… [+1735 chars]"
-  },
-  {
-      "source": {
-          "id": null,
-          "name": "NDTV News"
-      },
-      "author": "NDTV Sports Desk",
-      "title": "Hardik Faces Huge Trolling For \"Enjoy From Other End\" Message To Arshdeep",
-      "description": "Indian cricket team all-rounder Hardik Pandya faced massive trolling on social media for his message to Arshdeep Singh during the second T20I encounter against South Africa.",
-      "url": "https://sports.ndtv.com/south-africa-vs-india-2024/hardik-pandya-faces-huge-trolling-for-enjoy-from-other-end-message-to-arshdeep-singh-6988684",
-      "urlToImage": "https://c.ndtvimg.com/2024-11/vq60i1ko_cas_625x300_10_November_24.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738",
-      "publishedAt": "2024-11-11T01:45:26Z",
-      "content": "Indian cricket team all-rounder Hardik Pandya faced massive trolling on social media for his message to Arshdeep Singh during the second T20I encounter against South Africa on Sunday. India suffered … [+2005 chars]"
-  }
-  ];
+ 
 
   constructor() {
     super();
     this.state = {
-      article: this.article,
+      articles: [],
       loading: false,
     };
   }
+async componentDidMount() {
+  let url="https://newsapi.org/v2/everything?q=headlines=2024-11-15&sortBy=popularity&apiKey=6b87c57f5027403f9d5dc158704c7f9c";
+  let data = await fetch(url);
+  let parseData= await data.json();
+  console.log(parseData)
+  this.setState({articles:parseData.articles}); 
+}
+
+
   render() {
     return (
       <>
         <div className="container text-center my-3">
           <div className="row">
-            {this.state.article.map((element) => {
+            {this.state.articles.map((element) => {
               return (
                 <div className="col-4" key={element.title}>
                   <News
